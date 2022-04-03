8dcef340d61e7047e5497373ea8bbcf307370e43
Stian Rognhaugen <stian.rognhaugen@gmail.com>
2022-04-03 19:51:49 +0200

    --

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import * as React from "react"
 
 import Layout from "../components/layout"
-import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
     <h1 id="title"></h1>
   </Layout>
 )
